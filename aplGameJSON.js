cb6647356d77e6efad529a683c8ed343a4a86eea
rojo2010 <71035530+rojo2010@users.noreply.github.com>
2023-05-31 13:45:25 -0700

    Add files via upload

---
+++ aplGameJSON.js
@@ -0,0 +1,45 @@
+DATA = [
+	{
+		case: "case0"
+		imageType: "cellavision",
+		caseType: "meta",
+		imageCount: 47,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0"
+		imageType: "cellavision",
+		caseType: "myelo",
+		imageCount: 47,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0"
+		imageType: "cellavision",
+		caseType: "pro",
+		imageCount: 47,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case1"
+		imageType: "cellavision",
+		caseType: "meta",
+		imageCount: 52,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case1"
+		imageType: "cellavision",
+		caseType: "myelo",
+		imageCount: 52,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case1"
+		imageType: "cellavision",
+		caseType: "pro",
+		imageCount: 52,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}];
+
---
+++ aplGameJSON2.js
@@ -0,0 +1,76 @@
+DATA = [
+	{
+		case: "case0",
+		diagnosis: "BALL",
+		imageType: "cellavision",
+		caseType: "nAPL",
+		imageCount: 47,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case1",
+		diagnosis: "AML NOS",
+		imageType: "cellavision",
+		caseType: "nAPL",
+		imageCount: 52,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case2",
+		diagnosis: "BALL",
+		imageType: "cellavision",
+		caseType: "nAPL",
+		imageCount: 104,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case4",
+		diagnosis: "B-ALL",
+		imageType: "cellavision",
+		caseType: "nAPL",
+		imageCount: 99,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case5",
+		diagnosis: "B-ALL with BCR ABL",
+		imageType: "cellavision",
+		caseType: "nAPL",
+		imageCount: 80,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case6",
+		diagnosis: "Therapy related AML",
+		imageType: "cellavision",
+		caseType: "nAPL",
+		imageCount: 62,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case7",
+		diagnosis: "AML with KMT2a",
+		imageType: "cellavision",
+		caseType: "nAPL",
+		imageCount: 106,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case8",
+		diagnosis: "AML with BCR ABL",
+		imageType: "cellavision",
+		caseType: "nAPL",
+		imageCount: 105,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case9",
+		diagnosis: "AML with maturation, NOS",
+		imageType: "cellavision",
+		caseType: "nAPL",
+		imageCount: 87,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}
+];
+
