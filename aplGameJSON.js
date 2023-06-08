e1e998da6426df6b7f3490f9c01c307b3f2d5703
rojo2010 <71035530+rojo2010@users.noreply.github.com>
2023-06-07 22:55:05 -0700

    Add files via upload
    New version

--- aplGameJSON.js
+++ aplGameJSON.js
@@ -2,43 +2,92 @@ DATA = [
 	{
 		case: "case0",
 		imageType: "cellavision",
+		caseType: "smudge",
+		imageCount: 3,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0",
+		imageType: "cellavision",
+		caseType: "plasma",
+		imageCount: 1,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0",
+		imageType: "cellavision",
+		caseType: "mega",
+		imageCount: 1,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0",
+		imageType: "cellavision",
+		caseType: "lym",
+		imageCount: 3,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0",
+		imageType: "cellavision",
+		caseType: "neut",
+		imageCount: 3,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0",
+		imageType: "cellavision",
+		caseType: "hist",
+		imageCount: 1,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0",
+		imageType: "cellavision",
+		caseType: "eryth",
+		imageCount: 8,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0",
+		imageType: "cellavision",
+		caseType: "eo",
+		imageCount: 3,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0",
+		imageType: "cellavision",
+		caseType: "blast",
+		imageCount: 3,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0",
+		imageType: "cellavision",
+		caseType: "band",
+		imageCount: 3,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0",
+		imageType: "cellavision",
 		caseType: "meta",
+		imageCount: 17,
+		// nAPLresponse: ""
+		// pAPLresponse:: ""
+	}, {
+		case: "case0",
+		imageType: "cellavision",
+		caseType: "myelo",
 		imageCount: 7,
 		// nAPLresponse: ""
 		// pAPLresponse:: ""
 	}, {
 		case: "case0",
 		imageType: "cellavision",
-		caseType: "myelo",
-		imageCount: 3,
-		// nAPLresponse: ""
-		// pAPLresponse:: ""
-	}, {
-		case: "case0",
-		imageType: "cellavision",
 		caseType: "pro",
-		imageCount: 8,
-		// nAPLresponse: ""
-		// pAPLresponse:: ""
-	}, {
-		case: "case1",
-		imageType: "cellavision",
-		caseType: "meta",
-		imageCount: 6,
-		// nAPLresponse: ""
-		// pAPLresponse:: ""
-	}, {
-		case: "case1",
-		imageType: "cellavision",
-		caseType: "myelo",
-		imageCount: 3,
-		// nAPLresponse: ""
-		// pAPLresponse:: ""
-	}, {
-		case: "case1",
-		imageType: "cellavision",
-		caseType: "pro",
-		imageCount: 4,
+		imageCount: 14,
 		// nAPLresponse: ""
 		// pAPLresponse:: ""
 	}];
